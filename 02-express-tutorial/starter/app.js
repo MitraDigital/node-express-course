cfb68b3357d65dfef54a6969b1e4ff71b4758305
Kushan Athukorala <kathukorala@mitrai.com>
2026-02-10 08:41:45 +0530

    add

--- 02-express-tutorial/starter/app.js
+++ 02-express-tutorial/starter/app.js
@@ -1,147 +1,18 @@
-////// midleware
-/// LOGGER
-// morgan middleware
 const express = require('express')
 const app = express()
-const morgan = require('morgan')
+const auth = require('./routes/auth')
+const people = require('./routes/people')
 
-// app.use(morgan('tiny'))
-app.use(morgan('combined'))
+// static assets
+app.use(express.static('./methods-public'))
+// parse form data
+app.use(express.urlencoded({ extended: false }))
+// parse json
+app.use(express.json())
 
-app.get('/', (req, res) => {
-    res.send('Home')
+app.use('/api/people', people)
+app.use('/login', auth)
+
+app.listen(5000, () => {
+  console.log('Server is listening on port 5000....')
 })
-
-app.listen(5000)
-
-
-// const express = require('express')
-// const app = express()
-// const logger = require ('./logger')
-// app.use(logger)
-
-// app.get('/',  (req, res) => {
-//     res.send('Home')
-// })
- 
-
-// app.listen(5000)
-
-
-
-// const express = require('express')
-// const app = express()
-
-// const logger = (req, res, next) => {
-//     console.log(req.method)
-//     next()
-// }
-// app.get('/', logger, (req, res) => {
-//     res.send('Home')
-// })
-
-
-// app.listen(5000)
-
-
-
-
-// const express = require('express')
-// const app = express()
-// const { products } = require('./data.js') 
-
-// app.get('/api/products', (req, res) => {
-//     const newProducts = products.map((product => {
-//         const { id, name, image } = product
-//         return { id, name, image }
-//     }))
-//     res.status(200).json(newProducts)
-// })
-
-// app.get('/api/products/:productId', (req, res) => { 
-//     const { productId } = req.params
-//     const aProduct = products.find(product => product.id === Number(productId))
-//     if (!aProduct) {
-//         return res.status(404).send('Product does not exist')
-//     }
-//     return res.status(200).json(aProduct)
-// })
-
-// // search product
-// app.get('/api/v1/query', (req, res) => {
-//     console.log(req.query)
-//     const { search, limit } = req.query
-
-//     let sortedProducts = [...products]
-//     if (search) {
-//         sortedProducts = sortedProducts.filter(product => {
-//             return product.name.startsWith(search)
-//         }
-//         )
-//     }
-
-//     if (limit) {
-//         sortedProducts = sortedProducts.slice(0, Number(limit))
-//     }
-
-//     return res.status(200).json(sortedProducts)
-
-// })
-
-// app.listen(5000)
-
-
-// // products data
-// const express = require('express')
-// const app = express()
-// const { products }= require('./data.js')
-
-// app.get('/', (req, res) => {
-//     res.json(products)
-// })
-
-// app.listen(5000)
-
-
-
-// /////// json response
-// const express = require('express')
-// const app = express()
-
-// app.get('/', (req, res) => {
-//     res.json({"name":'john', 'age': 30})
-// })
-
-// app.listen(5000)
-
-// const express = require('express')
-// const app = express()
-
-// app.use(express.static('./public'))
-
-// app.get('/', (req, res) => {
-//     res.status(200).send('home')
-// })
-
-// app.all('*', () => {
-//     res.status(404).send('<h1> no resource </h1>')
-// })
-// app.listen(5000)
-
-// const http = require('http')
-// const { readFileSync } = require('fs')
-
-// // get all files
-
-
-// const server = http.createServer((req, res) => {
-//     const url = req.url
-//     console.log(url)
-//     if(url === '/') {
-//         res.writeHead(200, {'content-type': 'text/html'})
-//         res.write('homePage')
-//         res.end()
-//     }
-// })
-
-// server.listen(5000)
