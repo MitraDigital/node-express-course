4d09d736836b127ba76364811e483d12e1464187
Kushan Athukorala <kathukorala@mitrai.com>
2026-02-17 12:16:49 +0530

    Update main.js

--- 05-JWT-Basics/starter/controllers/main.js
+++ 05-JWT-Basics/starter/controllers/main.js
@@ -1,4 +1,5 @@
 const CustomAPIError = require('../errors/custom-error')
+const jwt = require('jsonwebtoken')
 
 const login = async (req, res) => {
     console.log(req.body)
@@ -8,10 +9,31 @@ const login = async (req, res) => {
         throw new CustomAPIError("no un or pw", 400)
     }
 
-    res.send('Fake Login')
+    const id = new Date().getDate()
+
+    const token = jwt.sign({id, username }, process.env.JWT_SECRET, {expiresIn:'30d'})
+    // console.log(token)
+
+    res.status(201).json({msg:'token created', token})
 }
 
+
 const dashboard = async (req, res) => {
+    const header = req.headers.authorization
+    console.log(header)
+    if (!header || !header.startsWith('Bearer ')){
+        throw new CustomAPIError("no token", 401)
+    }
+    const token = header.split(' ')[1]
+    console.log(token)
+    try {
+        jwt.verify(token, process.env.JWT_SECRET)
+    } catch( err) {
+        throw new CustomAPIError('invalid token', 401)
+        console.log(err)
+    }
+    
+
     res.send('Dashboard')
 }
 
