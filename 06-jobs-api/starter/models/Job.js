54640db762e913269612e5e198e28a94c6a07adc
Kushan Athukorala <kathukorala@mitrai.com>
2026-02-19 14:37:53 +0530

    add

--- 06-jobs-api/starter/models/Job.js
+++ 06-jobs-api/starter/models/Job.js
@@ -1,5 +1,26 @@
 const mongoose = require('mongoose')
 const { Schema } = mongoose
 const JobSchema = new Schema({
-    
-})
+    company: {
+        type: String,
+        required: [true, 'comp'],
+        maxlength: 50
+    },
+    position: {
+        type: String,
+        required: true,
+        maxlength: 100
+    },
+    status: {
+        type: String,
+        enum: ['interview', 'declined', 'pending'],
+        default: 'pending'
+    },
+    createdBy: {
+        type: mongoose.Types.ObjectId,
+        ref: 'User',
+        required: [true, 'user']
+    }
+}, {timestamps: true})
+
+module.exports = mongoose.model('Job', JobSchema)
